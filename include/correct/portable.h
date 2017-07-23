b63d4ac3e108955b423313b88f8f455405170347
Brian Armstrong <brian.armstrong.ece+github@gmail.com>
2017-07-22 22:34:38 -0700

    fix popcount impl

--- include/correct/portable.h
+++ include/correct/portable.h
@@ -12,7 +12,7 @@ static inline int popcount(int x) {
     /* taken from the helpful http://graphics.stanford.edu/~seander/bithacks.html#CountBitsSetParallel */
     x = x - ((x >> 1) & 0x55555555);
     x = (x & 0x33333333) + ((x >> 2) & 0x33333333);
-    return ((x + (x >> 4) & 0x0f0f0f0f) + 0x01010101) >> 24;
+    return ((x + (x >> 4) & 0x0f0f0f0f) * 0x01010101) >> 24;
 }
 
 static inline void prefetch(void *x) {}
