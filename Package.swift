303e77bdde041d02e8fac71746b7f601b7da7021
Aaron Farnham <aaron@foreflight.com>
2021-08-31 08:28:54 -0500

    First attempt at a swift package

---
+++ Package.swift
@@ -0,0 +1,47 @@
+// swift-tools-version:5.4
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "libcorrect",
+    platforms: [.iOS(.v13), .macOS(.v10_14)],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(name: "libcorrect",
+                 targets: ["libcorrect"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "libcorrect",
+            dependencies: [],
+            path:".",
+            exclude: [
+                "util",
+                "tests",
+                "tools",
+                "CMakeLists.txt",
+                "src/CMakeLists.txt",
+                "src/reed-solomon/CMakeLists.txt",
+                "src/convolutional/CMakeLists.txt",
+                "README.md",
+                "appveyor.yml",
+                "LICENSE",
+                
+                //requires x86
+                "src/convolutional/sse",
+                "include/correct-sse.h",
+                "include/correct/convolutional/sse",
+                "include/correct/util",
+
+            ],
+            publicHeadersPath: "include"
+        ),
+    ]
+)
